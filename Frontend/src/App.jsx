be4bd902fb31a45d60ad6c43ee93c4180fec08ef
Raj Rasane <rasaneraj04@gmail.com>
2024-10-20 17:19:54 +0530

    created navbar component for the project

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,8 @@
+import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <h1 className="text-xl font-bold">Welcome to House Price Prediction!</h1>
-    </div>
+    <Navbar/>
   );
 }
 
