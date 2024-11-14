047464df3d0303d1f3cecfcd414c7555bdfae30b
Raj Rasane <rasaneraj04@gmail.com>
2024-11-14 19:08:34 +0530

    Create UserAuthForm component with dynamic rendering for Register/Sign In forms.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'; // Import React
-import Navbar from "./components/Navbar"; // Import Navbar component
+import Navbar from "./components/navbar";
 import { Route, Routes } from "react-router-dom"; // Import Route and Routes for routing
-import UserAuthForm from "./pages/UserAuthForm"; // Import UserAuthForm for sign-in and register pages
+import UserAuthForm from "./pages/userauthform"; // Import UserAuthForm for sign-in and register pages
 import FinalForm from './components/finalform';
 import { Footer } from './components/footer';
 import NotFound from './pages/NotFound';
@@ -18,8 +18,8 @@ function App() {
         <Route path="/" element={<FinalForm/>} />
 
         {/* Authentication Routes */}
-        <Route path="/signin" element={<UserAuthForm type="signin" />} />
-        <Route path="/register" element={<UserAuthForm type="register" />} />
+        <Route path="/signin" element={<UserAuthForm />} />
+        <Route path="/register" element={<UserAuthForm isRegister={true} />} />
 
         {/* Catch-all Route for 404 - Page Not Found */}
         <Route path="*" element={<NotFound/>} />
